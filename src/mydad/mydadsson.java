40d6016fdf2360217804a4748aeb9917b7851cbd
Evan Cann <sEvanCann@yccc.edu>
2018-09-05 19:10:21 -0400

    first commit

---
+++ src/mydad/mydadsson.java
@@ -0,0 +1,17 @@
+package mydad;
+
+public class mydadsson {
+	
+	//An Object is 2 things
+	//1. Actions.  Methods
+	//2. Attributes.  Variables.
+	private String name = "Mike";
+	//main method.  Starts the program.
+	//Method Signature-  What makes this method unique.  Defines the method.
+	public static void main(String[] args) 
+{
+	// Prints to the console.
+	System.out.println("Hello World");
+	System.out.println("My first java app");
+}
+}
